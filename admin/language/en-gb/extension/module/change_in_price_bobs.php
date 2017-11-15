19c35e34c80adcfef0d5d68bb420529f7bc10d14
Ivan <ivan.sav.86@gmail.com>
2017-11-15 14:55:01 +0300

    Конечный?

---
+++ admin/language/en-gb/extension/module/change_in_price_bobs.php
@@ -0,0 +1,50 @@
+<?php
+$_['author_label'] = 'Mr. Bobs module';
+$_['text_module']      = 'modules';
+$_['heading_title']       	  = 'Mass changes in prices, quantities, etc., from Mr. Bobs';
+
+// Column
+$_['column_name']          			= 'Product name';
+$_['column_category']          			= 'Category';
+$_['column_sort_order']          	= 'Sorting order';
+$_['column_discount']          	= 'Discounts on quantity';
+$_['column_action']          		= 'Action';
+$_['column_options']          	= 'Change options';
+$_['button_view']          			= 'View';
+$_['button_save']          			= 'Save all changes';
+
+$_['product_special_text']          			= 'Price per share:';
+$_['old_product_special_text']          			= 'The price of shares before the change:';
+$_['old_product_text']          			= 'Price before change:';
+$_['old_quantity_text']          			= 'Quantity before change:';
+$_['old_status_text']          			= 'Value before change:';
+$_['discount_text_quantity']          			= 'Quantity:';
+$_['discount_text_price']          			= 'Discounted price:';
+$_['entry_save']          			= 'Save price';
+$_['entry_edit_product']          			= 'Go to product editing';
+$_['entry_options_button']          	= 'Show product options';
+$_['entry_discount_button']          	= 'Show discounts for quantity';
+$_['entry_delete_discount']          	= 'Delete discount';
+$_['entry_delete_special']          	= 'Delete share';
+
+
+$_['success_update']          			= 'Prices are saved';
+$_['info_yes_base_price_factor']          			= 'Prices changed successfully';
+$_['info_no_base_price_factor']          			= 'Please set the ratio, for price changes';
+$_['info_yes_base_quantity_factor']          			= 'Quantity of products successfully changed';
+$_['info_no_base_quantity_factor']          			= 'Please set the factor to change the quantity of goods';
+$_['attentions_no_discount']          			= 'No discounts for quantity';
+$_['attentions_no_options']          			= 'Options to product was not created';
+$_['attentions_no_options_price']          			= 'No options affect the price';
+
+$_['help_base_price']          	= 'Change the prices in the selected products (including those that did not fit on this page) by a factor';
+$_['help_base_price_button']          	= 'Changes will immediately take effect';
+
+$_['help_base_quantity']          	= 'To change the number of items in the selected products (including those products that did not fit on the page) by a factor';
+$_['help_base_quantity_button']          	= 'Changes will immediately take effect';
+
+$_['text_confirm_quest']          	= 'Changes will immediately take effect. Are you sure?';
+
+
+
+?>
